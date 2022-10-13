673497d8105f6443fe32cf17d9535315c30ec719
Juan Ignacio Leyes Schneider <juanignacioleyes@gmail.com>
2022-10-13 16:41:31 -0300

    some changes in ContextSeeding Class

--- PeliculasApi/ContextSeeding.cs
+++ PeliculasApi/ContextSeeding.cs
@@ -8,72 +8,53 @@ namespace PeliculasApi
         public static async Task SeedTestData(this ApplicationDbContext context)
         {
 
-            //var rolAdminId = "9aae0b6d-d50c-4d0a-9b90-2a6873e3845d";
-            //var rolUserId = "3aecc270-8e17-4ab2-a8d0-5ea7e920d65a";
-            //var usuarioAdminId = "5673b8cf-12de-44f6-92ad-fae4a77932ad";
-            //var usuarioUserId = "8e19fb2b-c13a-4549-a4fa-9325bd387815";
+            var usuarioAdminId = "5673b8cf-12de-44f6-92ad-fae4a77932ad";
+            var usuarioUserId = "8e19fb2b-c13a-4549-a4fa-9325bd387815";
 
-            //var rolAdmin = new IdentityRole()
-            //{
-            //    Id = rolAdminId,
-            //    Name = "Admin",
-            //    NormalizedName = "Admin"
-            //};  
+            var passwordHasher = new PasswordHasher<IdentityUser>();
+
+            var username = "nacho@gmail.com";
+            var usernameUser = "user@gmail.com";
+
+            var usuarioAdmin = new IdentityUser()
+            {
+                Id = usuarioAdminId,
+                UserName = username,
+                NormalizedUserName = username,
+                Email = username,
+                NormalizedEmail = username,
+                PasswordHash = passwordHasher.HashPassword(null, "Aa5075!")
+            };  
             
-            //var rolUser = new IdentityRole()
-            //{
-            //    Id = rolUserId,
-            //    Name = "User",
-            //    NormalizedName = "User"
-            //};
+            var usuarioUser = new IdentityUser()
+            {
+                Id = usuarioUserId,
+                UserName = usernameUser,
+                NormalizedUserName = username,
+                Email = username,
+                NormalizedEmail = username,
+                PasswordHash = passwordHasher.HashPassword(null, "Aa5075!")
+            };
 
-            //var passwordHasher = new PasswordHasher<IdentityUser>();
+            var adminClaim = new IdentityUserClaim<string>()
+            {
+                Id = new Random().Next(),
+                ClaimType = ClaimTypes.Role,
+                UserId = usuarioAdminId,
+                ClaimValue = "Admin"
+            };
+            var userClaim = new IdentityUserClaim<string>()
+            {
+                Id = new Random().Next(),
+                ClaimType = ClaimTypes.Role,
+                UserId = usuarioUserId,
+                ClaimValue = "User"
+            };
 
-            //var username = "nacho@gmail.com";
-            //var usernameUser = "user@gmail.com";
+            await context.UserClaims.AddRangeAsync(new IdentityUserClaim<string>[] { userClaim, adminClaim });
+            await context.Users.AddRangeAsync(new IdentityUser[] { usuarioAdmin, usuarioUser });
 
-            //var usuarioAdmin = new IdentityUser()
-            //{
-            //    Id = usuarioAdminId,
-            //    UserName = username,
-            //    NormalizedUserName = username,
-            //    Email = username,
-            //    NormalizedEmail = username,
-            //    PasswordHash = passwordHasher.HashPassword(null, "Aa5075!")
-            //};  
-            
-            //var usuarioUser = new IdentityUser()
-            //{
-            //    Id = usuarioUserId,
-            //    UserName = usernameUser,
-            //    NormalizedUserName = username,
-            //    Email = username,
-            //    NormalizedEmail = username,
-            //    PasswordHash = passwordHasher.HashPassword(null, "Aa5075!")
-            //};
-
-            //context.Add(usuarioAdmin);
-            //context.Add(usuarioUser);
-
-            //context.Add(rolAdmin);
-            //context.Add(rolUser);
-
-            //context.Add(new IdentityUserClaim<string>()
-            //    {
-            //        Id = 1,
-            //        ClaimType = ClaimTypes.Role,
-            //        UserId = usuarioAdminId,
-            //        ClaimValue = "Admin"
-            //    });    
-            //context.Add(new IdentityUserClaim<string>()
-            //    {
-            //        Id = 1,
-            //        ClaimType = ClaimTypes.Role,
-            //        UserId = usuarioUserId,
-            //        ClaimValue = "User"
-            //    });
-
-            //await context.SaveChangesAsync();
+            await context.SaveChangesAsync();
         }
     }
 }
